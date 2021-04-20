0327c85c1612662bb47230c71c77c0b8d1da3504
dashakotova0702 <dashakotova0702@gmail.com>
2021-04-20 16:49:47 +0300

    laba

--- laba3.cpp
+++ laba3.cpp
@@ -4,9 +4,41 @@
 #include <regex>
 using namespace std;
 
+struct Node {
+        int vertex, weight;
+        Node* next;
+        Node (int _vertex, int _weight) : vertex(_vertex), weight(_weight), next(NULL) {
+        }
+};
+
 struct List {
-        pair <int, int> vrtx_wght;
-        List *next;
+        Node* first;
+        Node* last;
+        List() : first(NULL), last(NULL) {
+        }
+
+        bool is_empty () {
+                return (first == NULL);
+        }
+
+        void push_back (int vertex, int weight) {
+                Node* node = new Node(vertex, weight);
+                if (is_empty()) {
+                        first = node;
+                        last = node;
+                        return;
+                }
+                last->next = node;
+                last = node;
+        }
+        void print () {
+                if (is_empty()) return;
+                while (first) {
+                        cout << first->vertex << " " << first->weight << " ";
+                        first = first->next;
+                        cout << endl;
+                }
+        }
 };
 
 class Strategy {
@@ -67,9 +99,33 @@ private:
 List* arr_lsts;
 
 public:
-void init (ifstream &f)
-{
-        cout << "lists" << endl;
+void init (ifstream &f) {
+        int size;
+        string str;
+        smatch m;
+        getline(f, str);
+        size = stoi(str);
+        arr_lsts = new List[5];
+        while (getline(f, str)) {
+                int index, vertex, weight;
+                regex r1("\\:"), r2("\\("), r3("\\,"), r4("\\)");
+                regex_search(str, m, r1);
+                index = stoi(m.prefix());
+                while (m.suffix().length() != 0) {
+                        regex_search(str, m, r2);
+                        str = m.suffix();
+                        regex_search(str, m, r3);
+                        vertex = stoi(m.prefix());
+                        str = m.suffix();
+                        regex_search(str, m, r4);
+                        weight = stoi(m.prefix());
+                        arr_lsts[index].push_back(vertex, weight);
+                        str = m.suffix();
+                }
+                str.clear();
+        }
+        for (int i = 0; i < size; i++)
+                arr_lsts[i].print();
 }
 };
 
@@ -90,15 +146,39 @@ void init (ifstream &f) {
 }
 };
 
+void dkstr (Graph* g, ifstream &f, int start, int end) {
+        int size;
+        string str;
+        getline(f, str);
+        getline(f, str);
+        size = stoi(str);
+        int marks[size];
+        for (int i = 0; i < size; i++) {
+                if (i == start)
+                        marks[i] = 0;
+                else
+                        marks[i] = 100000;
+        }
+
+}
+
 int main (int argc, char const *argv[]) {
-        string type;
-        ifstream fout("graph.txt");
-        getline(fout, type);
-        Graph* g;
-        if (type == "matrix")
-                g = new Graph(new Matrix);
-        else
-                g = new Graph(new ArrayLists);
-        g->init(fout);
-        return 0;
+        if (argc == 3) {
+                int start = atoi(argv[1]);
+                int end = atoi(argv[2]);
+                string type;
+                ifstream fout("graph.txt");
+                getline(fout, type);
+                Graph* g;
+                if (type == "matrix")
+                        g = new Graph(new Matrix);
+                else
+                        g = new Graph(new ArrayLists);
+                g->init(fout);
+                dkstr(g, fout, start, end);
+                return 0;
+        }
+        else {
+                return -1;
+        }
 }
