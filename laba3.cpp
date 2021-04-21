a9358c46fd31ef85a2577414185b2729c5c4e390
Darya Kotova <dashakotova0702@gmail.com>
2021-04-22 00:30:30 +0300

    laba

--- laba3.cpp
+++ laba3.cpp
@@ -4,6 +4,15 @@
 #include <regex>
 using namespace std;
 
+class Marks {
+public:
+int* marks;
+int* processed_vertex;
+int* processed_vertex_marks;
+int size;
+int id_processed_vertex_marks = 0;
+};
+
 struct Node {
         int vertex, weight;
         Node* next;
@@ -32,58 +41,71 @@ struct List {
         }
         void print () {
                 if (is_empty()) return;
-                while (first) {
-                        cout << first->vertex << " " << first->weight << " ";
-                        first = first->next;
+                Node* node = first;
+                while (node) {
+                        cout << node->vertex << " " << node->weight << " ";
+                        node = node->next;
                         cout << endl;
                 }
         }
         int get_count_adj_vertex () {
                 int count = 0;
-                while (first) {
+                Node* node = first;
+                while (node) {
                         count++;
-                        first = first->next;
+                        node = node->next;
                 }
                 return count;
         }
         int get_adjacent_vertex (Marks* m) {
-                while (first) {
+                Node* node = first;
+                while (node) {
                         int a = 0, b = 0;
                         for (int i = 0; i < m->size; i++) {
-                                if (first->vertex == m->processed_vertex[i])
+                                if (node->vertex == m->processed_vertex[i])
                                         a++;
                         }
                         if (a == 0) {
                                 for (int i = 0; i < m->size; i++) {
-                                        if (first->vertex == m->processed_vertex_marks[i])
+                                        if (node->vertex == m->processed_vertex_marks[i])
                                                 b++;
                                 }
                                 if (b == 0) {
-                                        m->processed_vertex_marks[m->id_processed_vertex_marks] = first->vertex;
+                                        m->processed_vertex_marks[m->id_processed_vertex_marks] = node->vertex;
                                         m->id_processed_vertex_marks++;
-                                        return first->vertex;
+                                        return node->vertex;
                                 }
-                                else
-                                        first = first->next;
+                                else {
+                                        a = 0, b = 0;
+                                        node = node->next;
+                                }
                         }
-                        else
-                                first = first->next;
+                        else {
+                                a = 0, b = 0;
+                                node = node->next;
+                        }
+
                 }
+                return 0;
         }
-        int get_weight_vertex (int vertex, int adjacent_vertex) {
-                while (first) {
-                        if (first->vertex == adjacent_vertex)
-                                return first->weight;
+        int get_weight_vertex (int adjacent_vertex) {
+                Node* node = first;
+                while (node) {
+                        if (node->vertex == adjacent_vertex) {
+                                return node->weight;
+                        }
+                        node = node->next;
                 }
+                return 0;
         }
 };
 
 class Strategy {
 public:
 virtual void init (ifstream &f) = 0;
-virtual int adjacent_vertex (Marks* m, int vertex);
-virtual int get_count_adj_vertex(int vertex);
-virtual int weight_vertex (int vertex, int adjacent_vertex);
+virtual int adjacent_vertex (Marks* m, int vertex) = 0;
+virtual int get_count_adj_vertex(Marks* m, int vertex) = 0;
+virtual int weight_vertex (Marks* m, int vertex, int adjacent_vertex) = 0;
 virtual ~Strategy () {
 }
 };
@@ -125,22 +147,43 @@ void init (ifstream &f) {
                 }
                 str.clear();
         }
-        for (int j = 0; j < size; j++) {
-                for (int i = 0; i < size; i++) {
-                        cout << matrix[j][i];
-                }
-                cout << endl;
-        }
 }
-
 int adjacent_vertex (Marks* m, int vertex) {
+        int a = 0, b = 0;
+        for (int i = 0; i < m->size; i++) {
+                if (matrix[vertex][i] != 0) {
+                        for (int j = 0; j < m->size; j++) {
+                                if (i == m->processed_vertex[j]) {
+                                        a++;
+                                }
+                        }
+                        if (a == 0) {
+                                for (int k = 0; k < m->size; k++) {
+                                        if (i == m->processed_vertex_marks[k]) {
+                                                b++;
+                                        }
+                                }
+                                if (b == 0) {
+                                        m->processed_vertex_marks[m->id_processed_vertex_marks] = i;
+                                        m->id_processed_vertex_marks++;
+                                        return i;
+                                }
+                        }
+                }
+                a = 0, b = 0;
+        }
         return 0;
 }
-int get_count_adj_vertex(int vertex) {
-        return 0;
+int get_count_adj_vertex (Marks* m, int vertex) {
+        int count = 0;
+        for (int i = 0; i < m->size; i++) {
+                if (matrix[vertex][i] != 0)
+                        count++;
+        }
+        return count;
 }
-int weight_vertex (int adjacent_vertex) {
-        return 0;
+int weight_vertex (Marks* m, int vertex, int adjacent_vertex) {
+        return matrix[vertex][adjacent_vertex];
 }
 };
 
@@ -155,7 +198,7 @@ void init (ifstream &f) {
         smatch m;
         getline(f, str);
         size = stoi(str);
-        arr_lsts = new List[5];
+        arr_lsts = new List[size];
         while (getline(f, str)) {
                 int index, vertex, weight;
                 regex r1("\\:"), r2("\\("), r3("\\,"), r4("\\)");
@@ -174,19 +217,15 @@ void init (ifstream &f) {
                 }
                 str.clear();
         }
-        for (int i = 0; i < size; i++)
-                arr_lsts[i].print();
 }
-
 int adjacent_vertex (Marks* m, int vertex) {
         return arr_lsts[vertex].get_adjacent_vertex(m);
 }
-
-int get_count_adj_vertex(int vertex) {
+int get_count_adj_vertex(Marks* m, int vertex) {
         return arr_lsts[vertex].get_count_adj_vertex();
 }
-int weight_vertex (int vertex, int adjacent_vertex) {
-        return arr_lsts[vertex].get_weight_vertex(vertex, adjacent_vertex);
+int weight_vertex (Marks* m, int vertex, int adjacent_vertex) {
+        return arr_lsts[vertex].get_weight_vertex(adjacent_vertex);
 }
 };
 
@@ -198,38 +237,27 @@ public:
 
 Graph (Strategy *strtg = nullptr) : strtg_(strtg) {
 }
-
 void init (ifstream &f) {
         this->strtg_->init(f);
 }
-
-int get_count_adj_vertex(int vertex) {
-        return this->strtg_->get_count_adj_vertex(vertex);
+int get_count_adj_vertex(Marks* m, int vertex) {
+        return this->strtg_->get_count_adj_vertex(m, vertex);
 }
-
 int adjacent_vertex (Marks* m, int vertex) {
         return this->strtg_->adjacent_vertex(m, vertex);
 }
-
-int weight_vertex (int vertex, int adjacent_vertex) {
-        return this->strtg_->weight_vertex(vertex, adjacent_vertex);
+int weight_vertex (Marks* m, int vertex, int adjacent_vertex) {
+        return this->strtg_->weight_vertex(m, vertex, adjacent_vertex);
 }
-
 ~Graph () {
         delete this->strtg_;
 }
 };
-class Marks {
-public:
-int* marks;
-int* processed_vertex;
-int* processed_vertex_marks;
-int size;
-int id_processed_vertex_marks = 0;
-};
 
 void dkstr (Graph* g, Marks* m, ifstream &f, int start, int end) {
-        int vertex = start, id_processed_vertex = 0;
+        f.clear();
+        f.seekg(0);
+        int vertex, id_processed_vertex = 0;
         string str;
         getline(f, str);
         getline(f, str);
@@ -243,11 +271,40 @@ void dkstr (Graph* g, Marks* m, ifstream &f, int start, int end) {
                 else
                         m->marks[i] = 100000;
         }
-        int count_adj_vertex = g->get_count_adj_vertex(vertex);
-        for (int i = 0; i < count_adj_vertex; i++) {
-                int adjacent_vertex = g->adjacent_vertex(m, vertex);
-                int weight = g->weight_vertex(vertex, adjacent_vertex);
+
+        while (id_processed_vertex != m->size) {
+                int min = 100000, min_id = 0;
+                for (int i = 0; i < m->size; i++) {
+                        if (m->marks[i] < min) {
+                                int a = 0;
+                                for (int j = 0; j < m->size; j++) {
+                                        if (i == m->processed_vertex[j])
+                                                a++;
+                                }
+                                if (a == 0) {
+                                        min = m->marks[i];
+                                        min_id = i;
+                                }
+                        }
+                }
+                vertex = min_id;
+                int count_adj_vertex = g->get_count_adj_vertex(m, vertex);
+                for (int i = 0; i < count_adj_vertex; i++) {
+                        int adjacent_vertex = g->adjacent_vertex(m, vertex);
+                        int weight_adjacent_vertex = g->weight_vertex(m, vertex, adjacent_vertex);
+                        int mark = m->marks[vertex] + weight_adjacent_vertex;
+                        if (mark < m->marks[adjacent_vertex])
+                                m->marks[adjacent_vertex] = mark;
+                }
+                m->id_processed_vertex_marks = 0;
+                for (int i = 0; i < m->size; i++) {
+                        m->processed_vertex_marks[i] = -1;
+                }
+                m->processed_vertex[id_processed_vertex] = vertex;
+                id_processed_vertex++;
         }
+        for (int i = 0; i < m->size; i++)
+                cout << i << " = " << m->marks[i] << endl;
 }
 
 int main (int argc, char const *argv[]) {
